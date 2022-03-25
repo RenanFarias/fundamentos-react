16e8a3d0d749d7d6190f72020f4096c951a5772d
RenanFarias <renan.ii@hotmail.com>
2022-03-25 14:44:33 -0300

    start allover + first component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css' //import relativo para elementos da mesma pasta
+import ReactDOM from 'react-dom' //para renderizar elementos
+import React from 'react'//para usar o JSX
 
+import Primeiro from './components/basics/Primeiro'
+
+
+
+/*Pega o elemnto do html (index.html) e adiciona o 
+texto no elemento
+
+const el = document.getElementById('root')
+ReactDOM.render('Olá React!!!', el)*/
+
+
+const tag = <strong>Olá React</strong>
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    //isso eh renderizado gracas ao JSX que converte via React para mostrar em tela
+    <div>
+       <Primeiro></Primeiro>       
+    </div>,
+    document.getElementById('root')
+)
